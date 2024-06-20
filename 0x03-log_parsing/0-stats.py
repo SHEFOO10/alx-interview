940624e18de40da08a1ed91a88fdfdd54804893b
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-20 13:30:50 +0000

    refactored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
 """ 0-stats """
 import sys
-import re
-import signal
 
 
-def print_stats(codes_hits, size):
-    sys.stdout.write(f'File size: {size}\n')
-    for status_code, hits in codes_hits.items():
-        sys.stdout.write(f'{status_code}: {hits}\n')
-    sys.stdout.flush()
-
-
-def handle_SIGINT(signum, frame):
-    print_stats(codes_hits, Total_size)
-
-
-signal.signal(signal.SIGINT, handle_SIGINT)
-
-codes_hits = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+codes_hits = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0}
 Total_size = 0
+
+
+def print_stats():
+    print('File size: {}'.format(Total_size))
+    for status_code, hits in codes_hits.items():
+        if hits > 0:
+            print('{}: {}'.format(status_code, hits))
+
+
 requests = 0
 
-for line in sys.stdin:
-    match = re.match(
-        r'^.+ - \[[0-9-]+ [\d:\.]+\]'
-        r' "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
-        line
-        )
-    if match:
-        requests += 1
-        status, file_size = [
-            int(match.group(1)),
-            int(match.group(2))
-        ]
-        codes_hits[status] += 1
-        Total_size += file_size
-        if requests % 10 == 0:
-            print_stats(codes_hits, Total_size)
+try:
+    for line in sys.stdin:
+        line_list = line.split()
+        if len(line_list) > 4:
+            requests += 1
+            status, file_size = line_list[-2], int(line_list[-1])
+            codes_hits[status] += 1
+            Total_size += file_size
+            if requests == 10:
+                print_stats()
+                requests = 0
+finally:
+    print_stats()
