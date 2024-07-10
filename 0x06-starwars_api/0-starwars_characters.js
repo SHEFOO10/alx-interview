e724449c49c6956902d778d19d7a60b1364a7ec2
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-07-10 18:44:34 +0000

    fix semistandard issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,36 +3,36 @@
 const request = require('request');
 
 if (process.argv.length < 3) {
-    console.log("You must pass film_id");
-    return;
+  console.log('You must pass filmId');
+  process.exit();
 }
 
-const film_id = process.argv[2];
+const filmId = process.argv[2];
 
-function fetch(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else if (response.statusCode !== 200) {
-                reject(new Error(`Request failed with status code ${response.statusCode}`));
-            } else {
-                resolve(JSON.parse(body));
-            }
-        });
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Request failed with status code ${response.statusCode}`));
+      } else {
+        resolve(JSON.parse(body));
+      }
     });
+  });
 }
 
-fetch(`https://swapi-api.alx-tools.com/api/films/${film_id}`)
-    .then(filmData => {
-        const characterPromises = filmData.characters.map(characterUrl => fetch(characterUrl));
-        return Promise.all(characterPromises);
-    })
-    .then(characters => {
-        characters.forEach(character => {
-            console.log(character.name);
-        });
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error.message);
+fetch(`https://swapi-api.alx-tools.com/api/films/${filmId}`)
+  .then(filmData => {
+    const characterPromises = filmData.characters.map(characterUrl => fetch(characterUrl));
+    return Promise.all(characterPromises);
+  })
+  .then(characters => {
+    characters.forEach(character => {
+      console.log(character.name);
     });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error.message);
+  });
